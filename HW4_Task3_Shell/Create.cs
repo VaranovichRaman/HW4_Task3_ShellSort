e2bf13f6c9f8ddbf8e44fba38fa861e1ad3c0e31
Raven Raven <raven_180691@mail.ru>
2019-09-30 23:21:37 +0300

    Add project files.

---
+++ HW4_Task3_Shell/Create.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_Task3_Shell
+{
+    class Create
+    {
+        public static int[] CreateArray()
+        {
+            Random numbers = new Random();
+            int[] array = new int[20];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = numbers.Next(0, 100);
+            }
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine($"\n");
+
+            return array;
+        }
+    }
+}
---
+++ HW4_Task3_Shell/Sort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_Task3_Shell
+{
+    class Sort
+    {
+        public static int Distance { get; set; }
+        public static void ShellSort(int[] array)
+        {
+            Distance = array.Length / 2;
+            while (Distance >= 1)
+            {
+                for (var i = Distance; i < array.Length; i++)
+                {
+                    var j = i;
+                    while ((j >= Distance) && (array[j - Distance] > array[j]))
+                    {
+                        var temp = array[j];
+                        array[j] = array[j - Distance];
+                        array[j - Distance] = temp;
+                        j = j - Distance;
+                    }
+                }
+
+                Distance = Distance / 2;
+            }
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
